75ebe2259b40ebd5cdb46b31cf0d3dcf803b33d3
v-marsh <vidarmarsh@gmail.com>
2025-02-24 17:34:33 +0000

    Completed general insert function for piece table

--- src/piece_table_rewrite.rs
+++ src/piece_table_rewrite.rs
@@ -48,6 +48,12 @@ struct Piece {
     pub content: PieceBuf,
 }
 
+impl Piece {
+    fn len(&self) -> usize {
+        self.stop.0 - self.start.0
+    }
+}
+
 struct WriteLocation {
     position: Position,
     piece_id: usize,
@@ -67,7 +73,7 @@ pub struct PieceTable {
     /// for increased speed of successive `insert` commands. Is set to
     /// `None` if an operation has changed the buffer such that the
     /// location must be re-computed. Note it is assumed that this only
-    /// refers to a piece which points to the `addition` buffer.
+    /// refers to a valid piece which points to the `addition` buffer.
     previous_write: Option<WriteLocation>,
 }
 
@@ -78,17 +84,17 @@ impl PieceTable {
         pieces.push(Piece { start: Position(0), stop: Position(s.len()), content: PieceBuf::ORIGINAL });
         Self { original: s, addition: String::new(), pieces, previous_write: None }
     }
+
     /// Insert `content` at `position`.
     ///
     /// This will insert content at the index given by position and
-    /// return the number of characters written. If position < 0 then
-    /// the content will be inserted at index 0 and if position is
-    /// greater than the length of the piece table then the content
-    /// will be appended to the end of the piece table.
-    pub fn insert(&mut self, position: Position, content: &str) ->
-        Result<usize, PieceTableError> {
+    /// return the number of characters written. If position is greater
+    /// than the length of the piece table then the content will be
+    /// appended to the end of the piece table.
+    pub fn insert(&mut self, position: Position, content: &str) {
 
-        // Continue writing from previous position
+        // Append to piece from last insert if a valid write_location
+        // exists.
         if let Some(write_location) = &self.previous_write {
             if position == write_location.position {
                 self.addition.push_str(content);
@@ -97,22 +103,25 @@ impl PieceTable {
                     // unwrap here since a valid write_location is
                     // assumed for speed.
                     .unwrap()
-                    .stop.change_position(content.len());
+                    .stop
+                    .change_position(content.len());
             }
         }
 
-        // STILL NEED TO SOLVE FOR GENERAL CASE CURRENT CODE IS OUT OF DATE
+        // General case: write to any position in the table
+        let mut position = position;
         let mut piece: Option<&Piece> = None;
         let mut piece_id: Option<usize> = None;
-        let mut piece_start_loc: Option<usize> = None;
-        let mut current_loc = 0;
-        let mut next_loc = 0;
+        let mut piece_start_loc: Option<Position> = None;
+        let mut current_loc = Position(0);
+        let mut next_loc = Position(0);
 
+        // Find the piece containing 'position' within its range
         for (id, _piece) in self.pieces.iter().enumerate() {
-            next_loc += _piece.stop - _piece.start;
+            next_loc = next_loc + _piece.stop - _piece.start;
             if next_loc >= position {
-                // This can always be safely unwrapped since id is
-                // bounded by the lenth of self.pieces
+                // This can always be safely unwrapped since 'id' is
+                // bounded by the length of self.pieces.
                 piece = Some(&self.pieces.get(id).unwrap());
                 piece_id = Some(id);
                 piece_start_loc = Some(current_loc);
@@ -121,37 +130,48 @@ impl PieceTable {
             current_loc = next_loc;
         }
 
-        let piece = piece.ok_or(PieceTableError::GotBadLoc)?;
-        let piece_id = piece_id.ok_or(PieceTableError::GotBadLoc)?;
-        let piece_start_loc = piece_start_loc.ok_or(PieceTableError::GotBadLoc)?;
+        let piece = piece.unwrap_or(&self.pieces.last().unwrap());
+        let piece_id = piece_id.unwrap_or(self.pieces.len() - 1);
+        let piece_start_loc = piece_start_loc.unwrap_or(
+            current_loc+Position(piece.len())
+        );
+        // Insert content at the end of the piece table if the position
+        // is larger than any valid value.
+        if position > next_loc { position = next_loc };
 
-        // If loc is in the middle of a piece then split piece before
-        // inputting.
-        if position != piece_start_loc + piece.len() {
+        // Determine location to insert the new piece
+        let new_piece_id: usize;
+        if position == piece_start_loc {
+            new_piece_id = piece_id;
+        } else if position == piece_start_loc + piece.stop - piece.start {
+            new_piece_id = piece_id + 1;
+        } else {
             let piece_loc = position - piece_start_loc;
-            self.split_piece(piece_id, piece_loc)
-                .map_err(|_| PieceTableError::GotBadLoc)?;
+            // Can unwrap here since function only fails if piece_id is
+            // out of range.
+            self.split_piece(piece_id, piece_loc).unwrap();
+            new_piece_id = piece_id + 1;
         }
 
-        let start = self.addition.len();
+        // Insert the new piece
+        let start = Position(self.addition.len());
         self.addition.push_str(content);
-        let stop = self.addition.len();
-        let n_chars = stop - start;
-
+        let stop = Position(self.addition.len());
+        let n_chars = stop.0 - start.0;
         let new_piece = Piece { start, stop, content: PieceBuf::ADDITION };
-        let new_piece_id = piece_id + 1;
         self.pieces.insert(new_piece_id, new_piece);
-        self.current_piece_id = new_piece_id;
 
-
-        Ok(n_chars)
+        // Update write location for faster insert
+        self.previous_write = Some(WriteLocation{
+            position,
+            piece_id: new_piece_id
+        });
     }
 
     /// Delete slices from `PieceTable`.
     ///
     /// This will delete the slice in the range [`start`,`end`). If
-    /// `start` >= `end` then nothing is deleted. If `start` < 0 then
-    /// will delete in the range [0, `end`). If `end` > buffer current
+    /// `start` >= `end` then nothing is deleted. If `end` > buffer current
     /// buffer length then will delete in range [`start`, end of buffer]
     pub fn delete(&mut self, start: Position, end: Position) {
         // Determine the piece to split and location in the piece
@@ -159,5 +179,15 @@ impl PieceTable {
 
     }
 
-    fn get_piece_from_position(&self, position: Position) -> (Piece, usize)
+    fn split_piece(&mut self, piece_id: usize, piece_position: Position) -> Result<(),()> {
+        let mut left = self.pieces.get_mut(piece_id).ok_or(())?;
+        let right = Piece{
+            start: left.start + piece_position,
+			stop: left.stop,
+			content: PieceBuf::ADDITION
+        };
+        left.stop = right.start;
+        self.pieces.insert(piece_id + 1, right);
+        Ok(())
+    }
 }
